b11e18c9a13c7670107170c1452a7a5762639ab8
seanomeara96 <seanom96@gmail.com>
2024-02-16 13:51:20 +0000

    added store hash

--- client.go
+++ client.go
@@ -6,11 +6,13 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"time"
 )
 
 type Client struct {
 	BaseURL    *url.URL
 	AuthToken  string
+	StoreHash  string
 	httpClient *http.Client
 	Version    int
 }
@@ -24,6 +26,7 @@ func NewClient(storeHash string, authToken string, version int) Client {
 	}
 	client.BaseURL = url
 	client.AuthToken = authToken
+	client.StoreHash = storeHash
 	client.httpClient = http.DefaultClient
 	client.Version = version
 	return client
@@ -42,7 +45,7 @@ func (c *Client) configureRequest(httpMethod string, relativeUrl string, payload
 	return req, nil
 }
 
-func (c *Client) Request(httpMethod string, relativeUrl string, payload []byte) (*http.Response, error) {
+func (c *Client) Request(httpMethod string, relativeUrl string, payload []byte, attempt int) (*http.Response, error) {
 	req, err := c.configureRequest(httpMethod, relativeUrl, payload)
 	if err != nil {
 		return nil, err
@@ -53,23 +56,27 @@ func (c *Client) Request(httpMethod string, relativeUrl string, payload []byte) 
 		return nil, err
 	}
 
-	//maybe inplement auto retry with delay if response 429 or 5xx
+	if (resp.StatusCode == 429 || resp.StatusCode/100 == 5) && attempt < 3 {
+		log.Printf("%s request to %s failed with status code %d. Retrying in 3 seconds.", httpMethod, relativeUrl, resp.StatusCode)
+		time.Sleep(3 * time.Second)
+		return c.Request(httpMethod, relativeUrl, payload, attempt+1)
+	}
 
 	return resp, nil
 }
 
 func (client *Client) Get(url string) (*http.Response, error) {
-	return client.Request("GET", url, []byte(""))
+	return client.Request("GET", url, []byte(""), 0)
 }
 
 func (client *Client) Put(url string, payload []byte) (*http.Response, error) {
-	return client.Request("PUT", url, payload)
+	return client.Request("PUT", url, payload, 0)
 }
 
 func (client *Client) Post(url string, payload []byte) (*http.Response, error) {
-	return client.Request("POST", url, payload)
+	return client.Request("POST", url, payload, 0)
 }
 
 func (client *Client) Delete(url string) (*http.Response, error) {
-	return client.Request("DELETE", url, []byte(""))
+	return client.Request("DELETE", url, []byte(""), 0)
 }
